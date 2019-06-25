f7c521f1abb7d75ac70ae8b73fdc9b715e7b030b
cadugithub <carlosk2k2@gmail.com>
2019-06-25 15:46:16 -0300

    Adicionando a interface real do header

--- interfacesad/src/components/Header/index.js
+++ interfacesad/src/components/Header/index.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React from "react";
+import "../Bootstrap/bootstrap.min.css";
+import {Link} from 'react-router-dom'
+import avalionIcon from '../../img/avalionoff.jpg'
 
-
-export default function Header(){
-return (
-    <h1>Header Component</h1>
-)
+export default function Header() {
+  return (
+    <div id="header">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarTogglerDemo01"
+          aria-controls="navbarTogglerDemo01"
+          aria-expanded="false"
+          aria-label="Alterna navegação"
+        >
+          <span class="navbar-toggler-icon" />
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <Link class="navbar-brand" to="/">
+            <img src={avalionIcon} alt="Logo do site" />
+          </Link>
+          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+              <a class="nav-link" to="/"> Inicio<span class="sr-only">(Página atual)</span></a>
+            </li>
+            <li class="nav-item active">
+              <Link class="nav-link" to="/functions">
+                Gerenciar<span class="sr-only">(Página atual)</span>
+              </Link>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <button class="btn btn-outline-danger my-2 my-sm-0" type="submit">
+              Sair
+            </button>
+          </form>
+        </div>
+      </nav>
+    </div>
+  );
 }
