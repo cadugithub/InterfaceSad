b9acbaa549d776c7fee8e4cd845209873a3caf9b
cadugithub <carlosk2k2@gmail.com>
2019-06-25 16:13:40 -0300

    BrowserRouter

--- interfacesad/src/App.js
+++ interfacesad/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Header from '../src/components/Header'
+import {BrowserRouter} from 'react-router-dom'
 function App() {
   return (
     <>
+    <BrowserRouter>
     <Header />
+    </BrowserRouter>
     </>
   );
 }
