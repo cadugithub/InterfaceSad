633a2228ef01393b7101b08db6fd59d9eaaca709
cadugithub <carlosk2k2@gmail.com>
2019-06-26 04:24:46 -0300

    Alterações básicas

--- interfacesad/src/App.js
+++ interfacesad/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from '../src/components/Header'
-import Home from '../src/components/Home'
 import Routes from './routes'
 import {BrowserRouter} from 'react-router-dom'
 function App() {
