591128256a67f3c1f9fcb5ad9226fe547359e4e6
cadugithub <carlosk2k2@gmail.com>
2019-06-26 00:59:50 -0300

    adiconando algumas rotas, bibliotecas e  algumas alterações no index

--- interfacesad/src/components/Home/index.js
+++ interfacesad/src/components/Home/index.js
@@ -1,29 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+
 export default function Home(){
     return (
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col p-3 display-4">
-            <p class="text-center">O que você deseja fazer? Clique em uma imagem.</p>
-            </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col p-3 display-4">
+                <p class="text-center">O que você deseja fazer? Clique em uma imagem.</p>
+                </div>
 
-            <div class="col">
-            <Link to="/"><img src="../img/aluno1.png" alt="Clique no aluno" usemap="#Map" class="border"/></Link>
-                <map name="Map" id="Map">
-                <Link to="/"><area alt="Aluno" title="Clique para gerenciar Aluno" href=" {% url 'gerenciar_aluno' %} " shape="rect" coords="2,1,511,511" /></Link>
+                <div class="col">
+                <Link to="/"><img src="../img/aluno1.png" alt="Clique no aluno" usemap="#Map" class="border"/></Link>
+                    <map name="Map" id="Map">
+                    <Link to="/manageStudents"><area alt="Aluno" title="Clique para gerenciar Aluno" href=" {% url 'gerenciar_aluno' %} " shape="rect" coords="2,1,511,511" /></Link>
+                    </map>
+                    <p ><strong>Gerenciar Alunos.</strong></p>
+                </div>
+                <div class="col">
+                <img src="../img/avaliacao.png" alt="Clique na avaliação" usemap="#Map1" class="border"/>
+                <map name="Map1" id="Map1">
+                <Link to="/"><area alt="Avaliacao" title="Clique para gerenciar avaliação diagnóstica" href="{% url 'gerenciar_avaliacao' %}" shape="rect" coords="2,0,510,511" /></Link>
                 </map>
-                <p ><strong>Gerenciar Alunos.</strong></p>
-            </div>
-            <div class="col">
-            <img src="../img/avaliacao.png" alt="Clique na avaliação" usemap="#Map1" class="border"/>
-            <map name="Map1" id="Map1">
-            <Link to="/"><area alt="Avaliacao" title="Clique para gerenciar avaliação diagnóstica" href="{% url 'gerenciar_avaliacao' %}" shape="rect" coords="2,0,510,511" /></Link>
-            </map>
-            <p><strong>Avaliação Diagnóstica.</strong></p>
+                <p><strong>Avaliação Diagnóstica.</strong></p>
+                </div>
             </div>
         </div>
-    </div>
     );
 }
--- interfacesad/src/routes.js
+++ interfacesad/src/routes.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Header from './components/Header'
+import ManageStudents from './pages/ManageStudents/manageStudents'
+import FormStudent from './pages/FormStudent/formStudent'
+import ManageEvaluations from '../src/pages/ManageEvaluation/ManageEvaluation'
+import DetailRating from '../src/pages/DetailsRating/DetailRating'
+import Home from './components/Home';
 
 export default function Routes (){
+    return(
     <Switch>
-    <Route exact path="/" component={Header}/>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/manageStudents" component={ManageStudents}/>
+        <Route exact path="/formStudent" component={FormStudent}/>
+        <Route exact path="/manageEvaluations" component ={ManageEvaluations}/>
+        <Route exact path="/detailRating/:id" component ={DetailRating}/>
     </Switch>
+    );
 }
 
