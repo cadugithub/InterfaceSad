c891c6807eb698a789b838dc55268a483626c97a
cadugithub <carlosk2k2@gmail.com>
2019-06-26 02:17:13 -0300

    Foi adicionado ação ao botão D(detalhes), ou seja, ela foi vinculado a sua respectiva rota

--- interfacesad/src/components/Actions/actions.js
+++ interfacesad/src/components/Actions/actions.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react"
 import {Link} from 'react-router-dom'
+import api from "../../services/api"
+
 export default function Action(props) {
 
+const [student, setStudents] = useState([])
+
+const loadStudent = async () =>{
+  const response = await api.get(`/aluno/${props.idStudent}/`)
+  setStudents(response.data);
+}
+
+useEffect(() => {
+  loadStudent();
+  console.log(props.idStudent)
+}, [])
  return props.result === null 
  ? (    <td class="acoes">
  <button type="button" className="btn btn-warning" id="alter">A</button>
  <button type="button" className="btn btn-danger">E</button>
- <Link to="/">
+ <Link to={`detailRating/${props.idStudent}`}>
 <button type="button" className="btn btn-info">D</button>
  </Link>
 </td>) 
@@ -15,9 +28,11 @@ export default function Action(props) {
    E
  </button>
  <Link to="/">
+ <Link to={`detailRating/${props.idStudent}`}>
    <button type="button" className="btn btn-info">
      D
    </button>
+</Link>
  </Link>
 </td>)
 }
