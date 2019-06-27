278c0be3f289a480da2d46d8f60f69dc22fe7ef0
cadugithub <carlosk2k2@gmail.com>
2019-06-27 11:50:47 -0300

    Foi adicionado a rota para o grafico

--- interfacesad/src/routes.js
+++ interfacesad/src/routes.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import Header from './components/Header'
 import ManageStudents from './pages/ManageStudents/manageStudents'
 import FormStudent from './pages/FormStudent/formStudent'
 import ManageEvaluations from '../src/pages/ManageEvaluation/ManageEvaluation'
 import DetailRating from '../src/pages/DetailsRating/DetailRating'
 import Home from './components/Home';
-
+import AnyChart from './components/AnyChart/anyChart'
 export default function Routes (){
     return(
     <Switch>
@@ -15,6 +14,7 @@ export default function Routes (){
         <Route exact path="/formStudent" component={FormStudent}/>
         <Route exact path="/manageEvaluations" component ={ManageEvaluations}/>
         <Route exact path="/detailRating/:id" component ={DetailRating}/>
+        <Route exact oath="/resultChart" component={AnyChart}/>
     </Switch>
     );
 }
