8ab635e86fdf756dfd00980c6d36cd83b1fcd497
cadugithub <carlosk2k2@gmail.com>
2019-06-28 09:02:13 -0300

    Foi adicionado os componentes resultado, observação e o botão salvar

--- interfacesad/src/components/AnyChart/anyChart.js
+++ interfacesad/src/components/AnyChart/anyChart.js
@@ -9,13 +9,39 @@ export default function ResultChart (){
             
             </div>
             <div className="col">
+            <div className="form-group">
+            <div className="col-12 col-sm-12">
                 <AnyChart
                     type="pie"
-                    data={[["Aprovado",0.3], ["Reprovado",0.0]]}
-                    title="Resultado"
-                    width={500}
-                    height={500}
+                    data={[["Aprovado",0.3], ["Reprovado",0.7]]}
+                    title="Carlos Eduardo Moreira Borges"
+                    width={300}
+                    height={300}
                 />
+              </div>
+                <div className="col-10 col-sm-10">
+                    <label for="exampleFormControlTextarea1">
+                    <b>Observação:</b>
+                    </label>
+                    <textarea className="col-12"
+                    className="form-control"
+                    id="exampleFormControlTextarea1"
+                    rows="3"
+                    />
+                </div>
+                <div className="col-10 col-sm-10">
+                    <label for="exampleFormControlTextarea1">
+                        <b>Resultado Final:</b>
+                    </label>
+                    <select class="form-control" id="finalResult">
+                        <option>Aprovado</option>
+                        <option>Reprovado</option>
+                    </select>
+                </div>
+                <div className="col-10 col-sm-10">
+                    <button  type="button" className="btn btn-primary">Salvar</button>
+                </div>
+            </div>
             </div>
             <div className="col-4">
             
