7d63414e8db0641b0625596b72385757aafd8551
cadugithub <carlosk2k2@gmail.com>
2019-06-30 08:14:34 -0300

    Foi feito um organização do codigo para melhor entendimento e organização da página

--- interfacesad/src/components/AnyChart/anyChart.js
+++ interfacesad/src/components/AnyChart/anyChart.js
@@ -1,25 +1,30 @@
 import React from 'react'
 import AnyChart from 'anychart-react'
 import '../Bootstrap/bootstrap.min.css'
+import './style.css'
 export default function ResultChart (){
     
     return(
         <div className="row">
-            <div className="col-sm-4">
+            <div className="col-sm-3">
             
             </div>
             <div className="col">
             <div className="form-group">
             <div className="col-12 col-sm-12">
+                    <p id="txtPrincipal">Analisando os dados de concelhos de classes anteriores, o aluno <u>Carlos Eduardo Moreira Borges</u> possui o seguinte percentual de classificação:</p>
+                </div>
+            <div className="col-12 col-sm-12" id="grafico">
                 <AnyChart
                     type="pie"
-                    data={[["Aprovado",0.3], ["Reprovado",0.7]]}
-                    title="Carlos Eduardo Moreira Borges"
-                    width={300}
-                    height={300}
+                    data={[["Deferido",0.3], ["Indeferido",0.7]]}
+                    title=""
+                    width={250}
+                    height={250}
                 />
+                <br/>
               </div>
-                <div className="col-10 col-sm-10">
+                <div className="col-12 col-sm-12">
                     <label for="exampleFormControlTextarea1">
                     <b>Observação:</b>
                     </label>
@@ -29,21 +34,22 @@ export default function ResultChart (){
                     rows="3"
                     />
                 </div>
-                <div className="col-10 col-sm-10">
+                <div className="col-12 col-sm-12">
                     <label for="exampleFormControlTextarea1">
                         <b>Resultado Final:</b>
                     </label>
                     <select class="form-control" id="finalResult">
-                        <option>Aprovado</option>
-                        <option>Reprovado</option>
+                        <option></option>
+                        <option>Deferido</option>
+                        <option>Indeferido</option>
                     </select>
                 </div>
-                <div className="col-10 col-sm-10">
-                    <button  type="button" className="btn btn-primary">Salvar</button>
+                <div className="col-12 col-sm-12" id="divSalvar">
+                    <button  type="button" className="btn btn-primary" id="btnSalvar">Salvar</button>
                 </div>
             </div>
             </div>
-            <div className="col-4">
+            <div className="col-3">
             
             </div>
         </div>
